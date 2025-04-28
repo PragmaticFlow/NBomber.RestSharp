4de7d8cc222928144821ed190b7b1034fb2afbbb
User <oleksandr.palamarchuk1310@gmail.com>
2025-04-28 14:47:03 +0300

    Add the demo project

---
+++ examples/Demo/PingPongExample.cs
@@ -0,0 +1,28 @@
+﻿using RestSharp;
+using NBomber.CSharp;
+using NBomber.RestSharp;
+
+new PingPongExample().Run();
+
+public class PingPongExample
+{
+    public void Run()
+    {
+        var scenario = Scenario.Create("restsharp_scenario", async ctx =>
+        {
+            var options = new RestClientOptions("https://reqres.in");
+            var client = new RestClient(options);
+            var request = new RestRequest("/api/users/2");
+
+            return await client.Send(request);
+        })
+        .WithoutWarmUp()
+        .WithLoadSimulations(
+            Simulation.KeepConstant(1, TimeSpan.FromSeconds(30))
+        );
+
+        NBomberRunner
+            .RegisterScenarios(scenario)
+            .Run();        
+    }
+}
