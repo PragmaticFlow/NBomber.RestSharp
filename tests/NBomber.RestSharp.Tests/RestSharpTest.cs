6a1ef0203e27d343aafc181181e59ddd7a15005d
User <oleksandr.palamarchuk1310@gmail.com>
2025-04-28 16:19:41 +0300

    Change testing resource for examples to the WebAppSimulator

--- tests/NBomber.RestSharp.Tests/RestSharpTest.cs
+++ tests/NBomber.RestSharp.Tests/RestSharpTest.cs
@@ -9,11 +9,14 @@ public class RestSharpTest
     [Fact]
     public void EndToEnd()
     {
+        // For this example, you'll need to start the HttpApiSimulator, which is located in the examples/simulators solution folder.
+        // Make sure it’s running before executing the client tests to ensure proper communication.
+
         var scenario = Scenario.Create("restsharp_scenario", async ctx =>
         {
-            var options = new RestClientOptions("https://reqres.in");
+            var options = new RestClientOptions("http://localhost:5099");
             var client = new RestClient(options);
-            var request = new RestRequest("/api/users/2");
+            var request = new RestRequest("/api/pingpong/");
 
             return await client.Send(request);
         })
