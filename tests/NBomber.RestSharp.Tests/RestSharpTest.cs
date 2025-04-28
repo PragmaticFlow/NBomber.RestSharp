d32019fd778c825a6bd5c91a9d0993b3ff9e1e71
User <oleksandr.palamarchuk1310@gmail.com>
2025-04-28 15:06:48 +0300

    Add tests

---
+++ tests/NBomber.RestSharp.Tests/RestSharpTest.cs
@@ -0,0 +1,38 @@
+﻿using RestSharp;
+using NBomber.CSharp;
+using NBomber.RestSharp;
+
+namespace Tests.RestSharp;
+
+public class RestSharpTest
+{
+    [Fact]
+    public void EndToEnd()
+    {
+        var scenario = Scenario.Create("restsharp_scenario", async ctx =>
+        {
+            var options = new RestClientOptions("https://reqres.in");
+            var client = new RestClient(options);
+            var request = new RestRequest("/api/users/2");
+
+            return await client.Send(request);
+        })
+        .WithWarmUpDuration(TimeSpan.FromSeconds(5))
+        .WithLoadSimulations(
+            Simulation.KeepConstant(1, TimeSpan.FromSeconds(5))
+        );
+
+        var stats = NBomberRunner
+            .RegisterScenarios(scenario)
+            .Run();
+
+        Assert.True(stats.AllOkCount > 0);
+        Assert.True(stats.AllFailCount == 0);
+
+        foreach (var scenarioStats in stats.ScenarioStats)
+        {
+            foreach (var stepStats in scenarioStats.StepStats)
+                Assert.True(stepStats.Ok.Latency.MinMs > 0);
+        }
+    }
+}
