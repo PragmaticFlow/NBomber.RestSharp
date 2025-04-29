0e31bd29da393c53a359a7b6068208055248678b
User <oleksandr.palamarchuk1310@gmail.com>
2025-04-29 15:12:12 +0300

    Add docs for the public methods

--- src/NBomber.RestSharp/RestClientExtensions.cs
+++ src/NBomber.RestSharp/RestClientExtensions.cs
@@ -13,8 +13,12 @@ public static class RestClientExtensions
     static readonly int SpaceLength = 1;
     static readonly int HttpVersionHeaderLength = 8;
     static readonly int HostHeaderLength = 4;
-    static readonly int StatusCodeLength = 3;    
+    static readonly int StatusCodeLength = 3;
 
+    /// <summary>
+    /// Sends an asynchronous HTTP request using the specified <see cref="RestRequest"/>,
+    /// then evaluates the response and wraps it in a <see cref="Response{RestResponse}"/> object, including size and latency metrics.
+    /// </summary>
     public static async Task<Response<RestResponse>> Send(this RestClient client, RestRequest request)
     {
         var response = await client.ExecuteAsync(request);
@@ -26,6 +30,11 @@ public static class RestClientExtensions
             return Response.Fail(payload: response, sizeBytes: sizeBytes);
     }
 
+    /// <summary>
+    /// Sends an asynchronous HTTP request using the specified <see cref="RestRequest"/>, 
+    /// deserializes the JSON response content into a specified type, then evaluates the response and wraps it
+    /// in a custom <see cref="Response{T}"/> object, including size and latency metrics.
+    /// </summary>
     public static async Task<Response<TResponse>> Send<TResponse>(this RestClient client, RestRequest request)
     {
         var response = await client.ExecuteAsync(request);
@@ -40,6 +49,12 @@ public static class RestClientExtensions
             return Response.Fail(payload: payload, sizeBytes: sizeBytes);
     }
 
+    /// <summary>
+    /// Sends an asynchronous HTTP GET request using the specified <see cref="RestRequest"/>, 
+    /// then evaluates the response and wraps it in a <see cref="Response{RestResponse}"/> object, including size and latency metrics.
+    /// <para />
+    /// The <see cref="RestRequest.Method"/> will be overwritten with <c>GET</c>.
+    /// </summary>
     public static async Task<Response<RestResponse>> SendGet(this RestClient client, RestRequest request)
     {
         request.Method = Method.Get;
@@ -47,6 +62,13 @@ public static class RestClientExtensions
         return await Send(client, request);
     }
 
+    /// <summary>
+    /// Sends an asynchronous HTTP GET request using the specified <see cref="RestRequest"/>, 
+    /// deserializes the JSON response content into a specified type, then evaluates the response 
+    /// and wraps it in a <see cref="Response{T}"/> object, including size and latency metrics.
+    /// <para />
+    /// The <see cref="RestRequest.Method"/> will be overwritten with <c>GET</c>.
+    /// </summary>
     public static async Task<Response<TResponse>> SendGet<TResponse>(this RestClient client, RestRequest request)
     {
         request.Method = Method.Get;
@@ -54,6 +76,12 @@ public static class RestClientExtensions
         return await Send<TResponse>(client, request);
     }
 
+    /// <summary>
+    /// Sends an asynchronous HTTP POST request using the specified <see cref="RestRequest"/>, 
+    /// then evaluates the response and wraps it in a <see cref="Response{RestResponse}"/> object, including size and latency metrics.
+    /// <para />
+    /// The <see cref="RestRequest.Method"/> will be overwritten with <c>POST</c>.
+    /// </summary>
     public static async Task<Response<RestResponse>> SendPost(this RestClient client, RestRequest request)
     {
         request.Method = Method.Post;
@@ -61,6 +89,13 @@ public static class RestClientExtensions
         return await Send(client, request);
     }
 
+    /// <summary>
+    /// Sends an asynchronous HTTP POST request using the specified <see cref="RestRequest"/>, 
+    /// deserializes the JSON response content into a specified type, then evaluates the response 
+    /// and wraps it in a <see cref="Response{T}"/> object, including size and latency metrics.
+    /// <para />
+    /// The <see cref="RestRequest.Method"/> will be overwritten with <c>POST</c>.
+    /// </summary>
     public static async Task<Response<TResponse>> SendPost<TResponse>(this RestClient client, RestRequest request)
     {
         request.Method = Method.Post;
@@ -68,6 +103,12 @@ public static class RestClientExtensions
         return await Send<TResponse>(client, request);
     }
 
+    /// <summary>
+    /// Sends an asynchronous HTTP PUT request using the specified <see cref="RestRequest"/>, 
+    /// then evaluates the response and wraps it in a <see cref="Response{RestResponse}"/> object, including size and latency metrics.
+    /// <para />
+    /// The <see cref="RestRequest.Method"/> will be overwritten with <c>PUT</c>.
+    /// </summary>
     public static async Task<Response<RestResponse>> SendPut(this RestClient client, RestRequest request)
     {
         request.Method = Method.Put;
@@ -75,6 +116,13 @@ public static class RestClientExtensions
         return await Send(client, request);
     }
 
+    /// <summary>
+    /// Sends an asynchronous HTTP PUT request using the specified <see cref="RestRequest"/>, 
+    /// deserializes the JSON response content into a specified type, then evaluates the response 
+    /// and wraps it in a <see cref="Response{RestResponse}"/> object, including size and latency metrics.
+    /// <para />
+    /// The <see cref="RestRequest.Method"/> will be overwritten with <c>PUT</c>.
+    /// </summary>
     public static async Task<Response<TResponse>> SendPut<TResponse>(this RestClient client, RestRequest request)
     {
         request.Method = Method.Put;
@@ -82,6 +130,12 @@ public static class RestClientExtensions
         return await Send<TResponse>(client, request);
     }
 
+    /// <summary>
+    /// Sends an asynchronous HTTP PATCH request using the specified <see cref="RestRequest"/>, 
+    /// then evaluates the response and wraps it in a <see cref="Response{RestResponse}"/> object, including size and latency metrics.
+    /// <para />
+    /// The <see cref="RestRequest.Method"/> will be overwritten with <c>PATCH</c>.
+    /// </summary>
     public static async Task<Response<RestResponse>> SendPatch(this RestClient client, RestRequest request)
     {
         request.Method = Method.Patch;
@@ -89,6 +143,13 @@ public static class RestClientExtensions
         return await Send(client, request);
     }
 
+    /// <summary>
+    /// Sends an asynchronous HTTP PATCH request using the specified <see cref="RestRequest"/>, 
+    /// deserializes the JSON response content into a specified type, then evaluates the response 
+    /// and wraps it in a <see cref="Response{RestResponse}"/> object, including size and latency metrics.
+    /// <para />
+    /// The <see cref="RestRequest.Method"/> will be overwritten with <c>PATCH</c>.
+    /// </summary>
     public static async Task<Response<TResponse>> SendPatch<TResponse>(this RestClient client, RestRequest request)
     {
         request.Method = Method.Patch;
@@ -96,6 +157,12 @@ public static class RestClientExtensions
         return await Send<TResponse>(client, request);
     }
 
+    /// <summary>
+    /// Sends an asynchronous HTTP DELETE request using the specified <see cref="RestRequest"/>, 
+    /// then evaluates the response and wraps it in a <see cref="Response{RestResponse}"/> object, including size and latency metrics.
+    /// <para />
+    /// The <see cref="RestRequest.Method"/> will be overwritten with <c>DELETE</c>.
+    /// </summary>
     public static async Task<Response<RestResponse>> SendDelete(this RestClient client, RestRequest request)
     {
         request.Method = Method.Delete;
@@ -103,6 +170,13 @@ public static class RestClientExtensions
         return await Send(client, request);
     }
 
+    /// <summary>
+    /// Sends an asynchronous HTTP DELETE request using the specified <see cref="RestRequest"/>, 
+    /// deserializes the JSON response content into a specified type, then evaluates the response 
+    /// and wraps it in a <see cref="Response{RestResponse}"/> object, including size and latency metrics.
+    /// <para />
+    /// The <see cref="RestRequest.Method"/> will be overwritten with <c>DELETE</c>.
+    /// </summary>
     public static async Task<Response<TResponse>> SendDelete<TResponse>(this RestClient client, RestRequest request)
     {
         request.Method = Method.Delete;
