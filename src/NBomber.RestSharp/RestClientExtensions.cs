25155dc37956581683fd1be1467c4a055f0ac1c4
User <oleksandr.palamarchuk1310@gmail.com>
2025-04-29 11:22:57 +0300

    Add calculation of the Host header to the GetRequestSize method

--- src/NBomber.RestSharp/RestClientExtensions.cs
+++ src/NBomber.RestSharp/RestClientExtensions.cs
@@ -11,13 +11,14 @@ public static class RestClientExtensions
     static readonly int HeaderSeparatorLength = 2;
     static readonly int CrlfLength = 2;
     static readonly int SpaceLength = 1;
-    static readonly int HttpPartLength = 8;
-    static readonly int StatusCodeLength = 3;
+    static readonly int HttpVersionHeaderLength = 8;
+    static readonly int HostHeaderLength = 4;
+    static readonly int StatusCodeLength = 3;    
 
     public static async Task<Response<RestResponse>> Send(this RestClient client, RestRequest request)
     {
         var response = await client.ExecuteAsync(request);
-        var sizeBytes = GetRequestSize(request) + GetResponseSize(response);
+        var sizeBytes = GetRequestSize(request, client.Options.BaseUrl.Authority) + GetResponseSize(response);
 
         if (response.IsSuccessStatusCode)
             return Response.Ok(payload: response, sizeBytes: sizeBytes);
@@ -28,7 +29,7 @@ public static class RestClientExtensions
     public static async Task<Response<TResponse>> Send<TResponse>(this RestClient client, RestRequest request)
     {
         var response = await client.ExecuteAsync(request);
-        var sizeBytes = GetRequestSize(request) + GetResponseSize(response);
+        var sizeBytes = GetRequestSize(request, client.Options.BaseUrl.Authority) + GetResponseSize(response);
 
         JsonSerializerOptions options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
         TResponse payload = JsonSerializer.Deserialize<TResponse>(response.Content, options);
@@ -109,7 +110,7 @@ public static class RestClientExtensions
         return await Send<TResponse>(client, request);
     }
 
-    private static long GetRequestSize(RestRequest request)
+    private static long GetRequestSize(RestRequest request, string host)
     {
         var sizeBytes = 0;
 
@@ -117,7 +118,9 @@ public static class RestClientExtensions
             .Sum(p => Encoding.UTF8.GetByteCount(p.Name) + HeaderSeparatorLength + Encoding.UTF8.GetByteCount(p.Value.ToString()) + CrlfLength);
 
         sizeBytes += Encoding.UTF8.GetByteCount(request.Method.ToString()) + SpaceLength + Encoding.UTF8.GetByteCount(request.Resource)
-            + SpaceLength + HttpPartLength + CrlfLength;
+            + SpaceLength + HttpVersionHeaderLength + CrlfLength;
+
+        sizeBytes += HostHeaderLength + HeaderSeparatorLength + Encoding.UTF8.GetByteCount(host) + CrlfLength;
 
         sizeBytes += CrlfLength;
 
@@ -131,7 +134,7 @@ public static class RestClientExtensions
         sizeBytes += response.Headers
             .Sum(p => Encoding.UTF8.GetByteCount(p.Name) + HeaderSeparatorLength + Encoding.UTF8.GetByteCount(p.Value) + CrlfLength);
 
-        sizeBytes += HttpPartLength + SpaceLength + StatusCodeLength + SpaceLength + Encoding.UTF8.GetByteCount(response.StatusCode.ToString())
+        sizeBytes += HttpVersionHeaderLength + SpaceLength + StatusCodeLength + SpaceLength + Encoding.UTF8.GetByteCount(response.StatusCode.ToString())
             + CrlfLength;
 
         sizeBytes += CrlfLength;
