7e10f5c8d48f503cbeef4e507c604f53a3ad22be
User <oleksandr.palamarchuk1310@gmail.com>
2025-04-29 11:00:38 +0300

    Remove client pool from the test

--- tests/NBomber.RestSharp.Tests/RestSharpTest.cs
+++ tests/NBomber.RestSharp.Tests/RestSharpTest.cs
@@ -1,5 +1,4 @@
 ﻿using RestSharp;
-using NBomber;
 using NBomber.CSharp;
 using NBomber.RestSharp;
 
@@ -13,33 +12,18 @@ public class RestSharpTest
         // For this example, you'll need to start the HttpApiSimulator, which is located in the examples/simulators solution folder.
         // Make sure it’s running before executing the client tests to ensure proper communication.
 
-        var clientPool = new ClientPool<RestClient>();
-
         var scenario = Scenario.Create("restsharp_scenario", async ctx =>
         {
-            var client = clientPool.GetClient(ctx.ScenarioInfo);
+            var options = new RestClientOptions("http://localhost:5099");
+            var client = new RestClient(options);
             var request = new RestRequest("/api/pingpong/");
 
             return await client.Send(request);
         })
-        .WithWarmUpDuration(TimeSpan.FromSeconds(5))
+        .WithoutWarmUp()
         .WithLoadSimulations(
-            Simulation.KeepConstant(1, TimeSpan.FromSeconds(5))
-        )
-        .WithInit(async context =>
-        {
-            for (var i = 0; i < 100; i++)
-            {
-                var options = new RestClientOptions("http://localhost:5099");
-                var client = new RestClient(options);
-                clientPool.AddClient(client);
-            }
-        })
-        .WithClean(ctx =>
-        {
-            clientPool.DisposeClients(client => client.Dispose());
-            return Task.CompletedTask;
-        });
+            Simulation.KeepConstant(10, TimeSpan.FromSeconds(5))
+        );
 
         var stats = NBomberRunner
             .RegisterScenarios(scenario)
