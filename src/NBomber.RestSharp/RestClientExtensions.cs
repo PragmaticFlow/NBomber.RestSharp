83533654327689f391cdf1996b8f7a1a965588dd
OleksPal <71185886+OleksPal@users.noreply.github.com>
2025-05-04 17:47:14 +0300

    Add restclient extensions (#1)

---
+++ src/NBomber.RestSharp/RestClientExtensions.cs
@@ -0,0 +1,220 @@
+﻿using System.Text.Json;
+using System.Text;
+using RestSharp;
+using NBomber.Contracts;
+using NBomber.CSharp;
+
+namespace NBomber.RestSharp;
+
+public static class RestClientExtensions
+{
+    static readonly int HeaderSeparatorLength = 2;
+    static readonly int CrlfLength = 2;
+    static readonly int SpaceLength = 1;
+    static readonly int HttpVersionHeaderLength = 8;
+    static readonly int HostHeaderLength = 4;
+    static readonly int StatusCodeLength = 3;
+
+    /// <summary>
+    /// Sends an asynchronous HTTP request using the specified <see cref="RestRequest"/>,
+    /// then evaluates the response and wraps it in a <c>Response&lt;RestResponse&gt;</c> object, including size and latency metrics.
+    /// </summary>
+    public static async Task<Response<RestResponse>> Send(this RestClient client, RestRequest request)
+    {
+        var response = await client.ExecuteAsync(request);
+        var sizeBytes = GetRequestSize(request, client.Options.BaseUrl.Authority) + GetResponseSize(response);
+
+        if (response.IsSuccessStatusCode)
+            return Response.Ok(payload: response, sizeBytes: sizeBytes);
+        else
+            return Response.Fail(payload: response, sizeBytes: sizeBytes);
+    }
+
+    /// <summary>
+    /// Sends an asynchronous HTTP request using the specified <see cref="RestRequest"/>, 
+    /// deserializes the JSON response content into a specified type, then evaluates the response and wraps it
+    /// in a custom <see cref="Response{T}"/> object, including size and latency metrics.
+    /// </summary>
+    public static async Task<Response<TResponse>> Send<TResponse>(this RestClient client, RestRequest request)
+    {
+        var response = await client.ExecuteAsync(request);
+        var sizeBytes = GetRequestSize(request, client.Options.BaseUrl.Authority) + GetResponseSize(response);
+
+        JsonSerializerOptions options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+        TResponse payload = JsonSerializer.Deserialize<TResponse>(response.Content, options);
+
+        if (response.IsSuccessStatusCode)
+            return Response.Ok(payload: payload, sizeBytes: sizeBytes);
+        else
+            return Response.Fail(payload: payload, sizeBytes: sizeBytes);
+    }
+
+    /// <summary>
+    /// Sends an asynchronous HTTP GET request using the specified <see cref="RestRequest"/>, 
+    /// then evaluates the response and wraps it in a <c>Response&lt;RestResponse&gt;</c> object, including size and latency metrics.
+    /// <para />
+    /// The <see cref="RestRequest.Method"/> will be overwritten with <c>GET</c>.
+    /// </summary>
+    public static async Task<Response<RestResponse>> SendGet(this RestClient client, RestRequest request)
+    {
+        request.Method = Method.Get;
+
+        return await Send(client, request);
+    }
+
+    /// <summary>
+    /// Sends an asynchronous HTTP GET request using the specified <see cref="RestRequest"/>, 
+    /// deserializes the JSON response content into a specified type, then evaluates the response 
+    /// and wraps it in a <see cref="Response{T}"/> object, including size and latency metrics.
+    /// <para />
+    /// The <see cref="RestRequest.Method"/> will be overwritten with <c>GET</c>.
+    /// </summary>
+    public static async Task<Response<TResponse>> SendGet<TResponse>(this RestClient client, RestRequest request)
+    {
+        request.Method = Method.Get;
+
+        return await Send<TResponse>(client, request);
+    }
+
+    /// <summary>
+    /// Sends an asynchronous HTTP POST request using the specified <see cref="RestRequest"/>, 
+    /// then evaluates the response and wraps it in a <c>Response&lt;RestResponse&gt;</c> object, including size and latency metrics.
+    /// <para />
+    /// The <see cref="RestRequest.Method"/> will be overwritten with <c>POST</c>.
+    /// </summary>
+    public static async Task<Response<RestResponse>> SendPost(this RestClient client, RestRequest request)
+    {
+        request.Method = Method.Post;
+
+        return await Send(client, request);
+    }
+
+    /// <summary>
+    /// Sends an asynchronous HTTP POST request using the specified <see cref="RestRequest"/>, 
+    /// deserializes the JSON response content into a specified type, then evaluates the response 
+    /// and wraps it in a <see cref="Response{T}"/> object, including size and latency metrics.
+    /// <para />
+    /// The <see cref="RestRequest.Method"/> will be overwritten with <c>POST</c>.
+    /// </summary>
+    public static async Task<Response<TResponse>> SendPost<TResponse>(this RestClient client, RestRequest request)
+    {
+        request.Method = Method.Post;
+
+        return await Send<TResponse>(client, request);
+    }
+
+    /// <summary>
+    /// Sends an asynchronous HTTP PUT request using the specified <see cref="RestRequest"/>, 
+    /// then evaluates the response and wraps it in a <c>Response&lt;RestResponse&gt;</c> object, including size and latency metrics.
+    /// <para />
+    /// The <see cref="RestRequest.Method"/> will be overwritten with <c>PUT</c>.
+    /// </summary>
+    public static async Task<Response<RestResponse>> SendPut(this RestClient client, RestRequest request)
+    {
+        request.Method = Method.Put;
+
+        return await Send(client, request);
+    }
+
+    /// <summary>
+    /// Sends an asynchronous HTTP PUT request using the specified <see cref="RestRequest"/>, 
+    /// deserializes the JSON response content into a specified type, then evaluates the response 
+    /// and wraps it in a <see cref="Response{T}"/> object, including size and latency metrics.
+    /// <para />
+    /// The <see cref="RestRequest.Method"/> will be overwritten with <c>PUT</c>.
+    /// </summary>
+    public static async Task<Response<TResponse>> SendPut<TResponse>(this RestClient client, RestRequest request)
+    {
+        request.Method = Method.Put;
+
+        return await Send<TResponse>(client, request);
+    }
+
+    /// <summary>
+    /// Sends an asynchronous HTTP PATCH request using the specified <see cref="RestRequest"/>, 
+    /// then evaluates the response and wraps it in a <c>Response&lt;RestResponse&gt;</c> object, including size and latency metrics.
+    /// <para />
+    /// The <see cref="RestRequest.Method"/> will be overwritten with <c>PATCH</c>.
+    /// </summary>
+    public static async Task<Response<RestResponse>> SendPatch(this RestClient client, RestRequest request)
+    {
+        request.Method = Method.Patch;
+
+        return await Send(client, request);
+    }
+
+    /// <summary>
+    /// Sends an asynchronous HTTP PATCH request using the specified <see cref="RestRequest"/>, 
+    /// deserializes the JSON response content into a specified type, then evaluates the response 
+    /// and wraps it in a <see cref="Response{T}"/> object, including size and latency metrics.
+    /// <para />
+    /// The <see cref="RestRequest.Method"/> will be overwritten with <c>PATCH</c>.
+    /// </summary>
+    public static async Task<Response<TResponse>> SendPatch<TResponse>(this RestClient client, RestRequest request)
+    {
+        request.Method = Method.Patch;
+
+        return await Send<TResponse>(client, request);
+    }
+
+    /// <summary>
+    /// Sends an asynchronous HTTP DELETE request using the specified <see cref="RestRequest"/>, 
+    /// then evaluates the response and wraps it in a <c>Response&lt;RestResponse&gt;</c> object, including size and latency metrics.
+    /// <para />
+    /// The <see cref="RestRequest.Method"/> will be overwritten with <c>DELETE</c>.
+    /// </summary>
+    public static async Task<Response<RestResponse>> SendDelete(this RestClient client, RestRequest request)
+    {
+        request.Method = Method.Delete;
+
+        return await Send(client, request);
+    }
+
+    /// <summary>
+    /// Sends an asynchronous HTTP DELETE request using the specified <see cref="RestRequest"/>, 
+    /// deserializes the JSON response content into a specified type, then evaluates the response 
+    /// and wraps it in a <see cref="Response{T}"/> object, including size and latency metrics.
+    /// <para />
+    /// The <see cref="RestRequest.Method"/> will be overwritten with <c>DELETE</c>.
+    /// </summary>
+    public static async Task<Response<TResponse>> SendDelete<TResponse>(this RestClient client, RestRequest request)
+    {
+        request.Method = Method.Delete;
+
+        return await Send<TResponse>(client, request);
+    }
+
+    private static long GetRequestSize(RestRequest request, string host)
+    {
+        var sizeBytes = 0;
+
+        sizeBytes += request.Parameters
+            .Sum(p => Encoding.UTF8.GetByteCount(p.Name) + HeaderSeparatorLength + Encoding.UTF8.GetByteCount(p.Value.ToString()) + CrlfLength);
+
+        sizeBytes += Encoding.UTF8.GetByteCount(request.Method.ToString()) + SpaceLength + Encoding.UTF8.GetByteCount(request.Resource)
+            + SpaceLength + HttpVersionHeaderLength + CrlfLength;
+
+        sizeBytes += HostHeaderLength + HeaderSeparatorLength + Encoding.UTF8.GetByteCount(host) + CrlfLength;
+
+        sizeBytes += CrlfLength;
+
+        return sizeBytes;
+    }
+
+    private static long GetResponseSize(RestResponse response)
+    {
+        var sizeBytes = 0;
+
+        sizeBytes += response.Headers
+            .Sum(p => Encoding.UTF8.GetByteCount(p.Name) + HeaderSeparatorLength + Encoding.UTF8.GetByteCount(p.Value) + CrlfLength);
+
+        sizeBytes += HttpVersionHeaderLength + SpaceLength + StatusCodeLength + SpaceLength + Encoding.UTF8.GetByteCount(response.StatusCode.ToString())
+            + CrlfLength;
+
+        sizeBytes += CrlfLength;
+
+        sizeBytes += response.RawBytes?.Length ?? 0;
+
+        return sizeBytes;
+    }
+}
---
+++ tests/NBomber.RestSharp.Tests/RestSharpTest.cs
@@ -0,0 +1,91 @@
+﻿using RestSharp;
+using NBomber.CSharp;
+using NBomber.RestSharp;
+
+namespace Tests.RestSharp;
+
+public class RestSharpTest
+{
+    [Fact]
+    public void EndToEnd()
+    {
+        // For this example, you'll need to start the HttpApiSimulator, which is located in the examples/simulators solution folder.
+        // Make sure it’s running before executing the client tests to ensure proper communication.
+
+        var scenario = Scenario.Create("restsharp_scenario", async ctx =>
+        {
+            var options = new RestClientOptions("http://localhost:5099");
+            var client = new RestClient(options);
+            var request = new RestRequest("/api/pingpong/");
+
+            var getStep = Step.Run("get", ctx, async () =>
+            {
+                var response = await client.SendGet(request);
+
+                if (response.Payload.Value.Content != "\"Get\"")
+                    throw new ArgumentException();
+
+                return response;
+            });
+
+            var postStep = Step.Run("post", ctx, async () =>
+            {
+                var response = await client.SendPost(request);
+
+                if (response.Payload.Value.Content != "\"Post\"")
+                    throw new ArgumentException();
+
+                return response;
+            });
+
+            var putStep = Step.Run("put", ctx, async () =>
+            {
+                var response = await client.SendPut(request);
+
+                if (response.Payload.Value.Content != "\"Put\"")
+                    throw new ArgumentException();
+
+                return response;
+            });
+
+            var patchStep = Step.Run("patch", ctx, async () =>
+            {
+                var response = await client.SendPatch(request);
+
+                if (response.Payload.Value.Content != "\"Patch\"")
+                    throw new ArgumentException();
+
+                return response;
+            });
+
+            var deleteStep = Step.Run("delete", ctx, async () =>
+            {
+                var response = await client.SendDelete(request);
+
+                if (response.Payload.Value.Content != "\"Delete\"")
+                    throw new ArgumentException();
+
+                return response;
+            });
+
+            return Response.Ok();
+        })
+        .WithWarmUpDuration(TimeSpan.FromSeconds(5))
+        .WithLoadSimulations(
+            Simulation.KeepConstant(10, TimeSpan.FromSeconds(5))
+        );
+
+        var stats = NBomberRunner
+            .RegisterScenarios(scenario)
+            .Run();
+
+        Assert.True(stats.AllOkCount > 0);
+        Assert.True(stats.AllFailCount == 0);
+
+        foreach (var scenarioStats in stats.ScenarioStats)
+        {
+            foreach (var stepStats in scenarioStats.StepStats)
+                Assert.True(stepStats.Ok.Latency.MinMs > 0);
+        }
+    }
+}
